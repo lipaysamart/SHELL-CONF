1f297b77f90a008322f0aeddf82e277277f44058
Ethan1437 <96456128+lipaysamart@users.noreply.github.com>
2023-03-08 15:33:20 -0800

    Update ping.sh

--- packge-tools/ping.sh
+++ packge-tools/ping.sh
@@ -2,16 +2,19 @@
 #####################
 # author: Ethan
 # email: lipaysamart@gmail.com
-# usage: 在虚拟机初始化时，固定你机器的IP地址脚本。
-# example：bash Bound_IP.sh 192.168.1.99 192.168.1.1
+# usage: 循环Ping IP地址脚本。
+# example：bash ping.sh 172.16.10.
 #####################
 
-for i in {1..254}
-do
-    ping -i -s 200 -c 1 ${1}.${i} &>/dev/null
-    if [[ $? -eq 0 ]];then
-        echo "${1}.${i}PING成功，网络通畅" >>/tmp/host_up.txt
-    else
-        echo "${1}.${i}失败，网络不通畅" >>/tmp/host_down.txt
-    fi
-done
+for i in {1..255}
+    do
+        #PING并保留丢包数
+        p=`ping -c 1 -w 1 ${1}.${i}|grep loss|awk '{print $6}'|awk -F "%" '{print $1}'`
+            #因为只PING一次，丢包数为0则表示成功，否则失败
+            if [ $p -eq 0 ]
+                then
+                    echo "${i}|PING成功，网络通畅" >> ./ipcheckdown.txt
+                else
+                    echo "${i}|失败，网络不通畅" >> ./ipcheckdown.txt
+            fi
+    done
